589b38bfe7a00e99ffdcf8672930e419e9da8d71
Stanley Sebastian <xxrena14@gmail.com>
2024-04-21 18:26:44 -0400

    finally

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Guide to Everything AI</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Replete-AI',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/gE3FVTKxKn',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Replete-AI',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'A Guide To Everything AI',
   },
 }
 
